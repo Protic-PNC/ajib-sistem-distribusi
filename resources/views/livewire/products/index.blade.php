d5a48a84a7919b427c0ee2b719b63e7a419109f7
Haruaki <32154385+haruaki07@users.noreply.github.com>
2023-09-04 00:58:36 +0000

    fix: table style

--- resources/views/livewire/products/index.blade.php
+++ resources/views/livewire/products/index.blade.php
@@ -1,42 +1,53 @@
-<div>
-
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-600">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Nama
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Harga
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Kategori
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Stok
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($products as $product)
-                    <tr class="bg-white border-b hover:bg-gray-50">
-                        <td class="px-6 py-3">
-                            {{ $product['name'] }}
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ formatIdr($product['harga']) }}
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ $product['category']['name'] }}
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ $product['stocks'][0]['quantity'] ?? 0 }}
-                        </td>
+<div class="max-w-screen-xl px-4">
+    <div class="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
+        <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+            <div>
+                <h4 class="text-gray-700 font-bold">Daftar Produk</h4>
+                <p class="text-xs text-gray-500">{{ count($products) }} hasil</p>
+            </div>
+            <div class="w-full md:w-72">
+                <form class="flex items-center">
+                    <label for="simple-search" class="sr-only">Search</label>
+                    <div class="relative w-full">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <x-phosphor-magnifying-glass class="w-5 h-5 text-gray-500" />
+                        </div>
+                        <input type="text" id="simple-search"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Search" required>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="overflow-x-auto">
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-4 py-3">Nama</th>
+                        <th scope="col" class="px-4 py-3">Harga</th>
+                        <th scope="col" class="px-4 py-3">Kategori</th>
+                        <th scope="col" class="px-4 py-3">Stok</th>
                     </tr>
-                @empty
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr class="border-b">
+                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">
+                                {{ $product['name'] }}
+                            </th>
+                            <td class="px-4 py-3">
+                                {{ formatIdr($product['harga']) }}
+                            </td>
+                            <td class="px-4 py-3">
+                                {{ $product['category']['name'] }}
+                            </td>
+                            <td class="px-4 py-3">
+                                {{ $product['stocks'][0]['quantity'] ?? 0 }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
